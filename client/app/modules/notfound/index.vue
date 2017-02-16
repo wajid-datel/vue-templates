061f644331d1e9c158768fe23cfd11cf1430a2e7
wajid-datel <wajid.khilji@datel.ee>
2017-02-16 15:10:26 +0200

    first

---
+++ client/app/modules/notfound/index.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+	.container
+		h2.title Error page
+
+</template>
+
+<script>
+	import { mapActions, mapGetters } from "vuex";
+
+	//import Service from "../../core/service";
+
+	export default {
+		/**
+		 * Computed getters
+		 */
+
+
+		/**
+		 * Page methods
+		 */
+		methods: {
+			/**
+			 * Actions from store
+			 */
+
+
+			/**
+			 * Increment counter
+			 */
+
+		},
+
+		/**
+		 * Socket handlers. Every property is an event handler
+		 */
+		socket: {
+
+			prefix: "/notfound/",
+
+			//namespace: "/counter",
+
+			events: {
+				/**
+				 * Counter value is changed
+				 * @param  {Number} msg Value of counter
+				 */
+
+			}
+		},
+
+	};
+
+</script>
+
+<style lang="sass" scoped>
+</style>
